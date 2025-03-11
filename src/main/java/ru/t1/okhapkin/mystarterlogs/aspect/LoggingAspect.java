0e8ecec746479abdfa4781e059292a71c9e3e09e
Mikhail Okhapkin <mikhail.okhapkin@yandex.ru>
2025-03-11 16:15:22 +0300

    Deleted an unnecessary annotation There was a component annotation in the class aspect, which was unnecessary.

--- src/main/java/ru/t1/okhapkin/mystarterlogs/aspect/LoggingAspect.java
+++ src/main/java/ru/t1/okhapkin/mystarterlogs/aspect/LoggingAspect.java
@@ -5,13 +5,11 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import ru.t1.okhapkin.mystarterlogs.component.HttpLoggingProperties;
 
 import java.util.Arrays;
 
 @Aspect
-@Component
 public class LoggingAspect {
 
     private final Logger logger = LoggerFactory.getLogger(LoggingAspect.class.getName());
